864ded5b3b71b96c65a2dd861bafae48727569b1
Andrew Chang <89715746+itsmandrew@users.noreply.github.com>
2026-04-01 20:08:39 -0700

    CRUD routes

---
+++ my_api/crud.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import Session
+from my_api import models, schemas
+
+
+def create_user(db: Session, user: schemas.UserCreate):
+    fake_hash_password = user.password + "_hashed"
+
+    db_user = models.User(
+        username=user.username, email=user.email, hashed_password=fake_hash_password
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+
+    return db_user
+
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
--- my_api/main.py
+++ my_api/main.py
@@ -1,10 +1,20 @@
-from fastapi import FastAPI
-from my_api.database import engine, Base
-from my_api import models
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from my_api.database import engine, Base, SessionLocal
+from my_api import models, schemas, crud
+
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/")
@@ -15,3 +25,17 @@ def root():
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+
+@app.post("/users", response_model=schemas.UserResponse)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    return crud.create_user(db, user)
+
+
+@app.get("/users/{user_id}", response_model=schemas.UserResponse)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return db_user
